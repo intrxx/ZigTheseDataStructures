84c8fc90adb485ee2e77aec70692d0a2d46a854b
Michał <michaloginski15@gmail.com>
2024-11-20 18:17:51 +0100

    Init with proper tool chain and zls.

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const vec = @import("vectors.zig");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
---
+++ src/vectors.zig
@@ -0,0 +1,10 @@
+
+pub const vector3 = struct {
+    pub var X = 0;
+    pub var Y = 0;
+    pub var Z = 0;
+};
+
+pub inline fn add() !vector3 {
+
+}
